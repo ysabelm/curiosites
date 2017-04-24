17ebf6dac61ad56d49613f8860bbc0f72598bc57
Isabelle M <ysabelmonfort@gmail.com>
2017-04-24 18:39:36 +0000

    Add array and variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    
+    @name = 'Grumpy cat'
+    @surname_1 = 'Aussi connu sous le nom de : Tardar sauce'
+    @surname_2 = 'Ou : Chat grincheux'
+    @description = 'Célébrité du web ravageusement boudeuse, Mème interstellaire'
+    @comment = 'Ce chat roxe du poney'
+    
+    @curiosities = [
+    'pelote de laine',
+    'tapis de souris',
+    'herbe à chat',
+    'arbre à gratter',
+    'oreilles de lapin',
+    'peluche dinosaure']
   end
 end
